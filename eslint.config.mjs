dc1f2f9d5e4329ef50a61270f4e297ac5f2cc02a
323905 <323905@example.com>
2026-02-22 16:36:18 +1300

    Fix missing Prisma ActionGetPayload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,7 @@ const eslintConfig = defineConfig([
     // Default ignores of eslint-config-next:
     ".next/**",
     ".next_backup_*/**",
+    ".next_buildcache_*/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
